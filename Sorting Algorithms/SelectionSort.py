ba3289c1a712764284723fe7859cd0c8ed1b0f26
Abhishek Chopra <coderabhish@gmail.com>
2024-07-10 23:58:07 +0530

    Selection Sorting - Recursive Method

--- Sorting Algorithms/SelectionSort.py
+++ Sorting Algorithms/SelectionSort.py
@@ -13,7 +13,25 @@ class SelectionSort:
             (lst[i], lst[minpos]) = (lst[minpos], lst[i])
         return lst
     #endregion
+    #region Recursive Method
+    def recursiveMthd(self, lst, start):
+        n = len(lst)
+        #Base condition
+        if n<1 or start >= n:
+            return lst
+        #logic
+        minpos = start
+        for i in range(start, n):
+            if lst[i] < lst[minpos]:
+                minpos = i
+        (lst[minpos], lst[start]) = (lst[start], lst[minpos])
+        #self call
+        return self.recursiveMthd(lst, start+1)
+    #endregion
+        
 
-unorderedLst = [3, 2, -1, 2, 4, 8]
+unorderedLst1 = [3, 2, -1, 2, 4, 8]
+unorderedLst2 = [10, 2, -7, 9, 3, 21]
 sort = SelectionSort()
-print(sort.iterativeMthd(unorderedLst))
+print(sort.iterativeMthd(unorderedLst1))
+print(sort.recursiveMthd(unorderedLst2, 0))
