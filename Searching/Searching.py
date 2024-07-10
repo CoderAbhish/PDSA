a22aa70383d7387689116c4339b693a00430864e
Abhishek Chopra <coderabhish@gmail.com>
2024-07-10 19:17:37 +0530

    Adding Comments and regions.

--- Searching/Searching.py
+++ Searching/Searching.py
@@ -1,5 +1,7 @@
+#Binary search (both Iterative and Recursive method)
 
 class BinarySearch:
+    #region Iterative Method
     def iterativeMthd(object, orderedLst, targetVal):
         n = len(orderedLst)
         if n < 1:
@@ -16,6 +18,8 @@ class BinarySearch:
                 
             mid = (lastIndex + firstIndex)//2
         return -1
+    #endregion
+    #region Recursive Method
     def recursiveMthd(object, orderedLst, targetVal, low, high):
         if high - low < 0:
             return -1
@@ -26,6 +30,7 @@ class BinarySearch:
             return(object.recursiveMthd(orderedLst, targetVal, mid+1, high))
         else:
             return(object.recursiveMthd(orderedLst, targetVal, low, mid-1))
+    #endregion
     
 search = BinarySearch()
 lst = [2, 4, 6, 7, 12, 15, 17]
