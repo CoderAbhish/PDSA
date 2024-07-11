b89241fa8115c67301585926d93dcee902da04a0
Abhishek Chopra <coderabhish@gmail.com>
2024-07-11 15:29:36 +0530

    Insertion Sort - Iterative Method

--- Sorting Algorithms/InsertionSort.py
+++ Sorting Algorithms/InsertionSort.py
@@ -1,6 +1,5 @@
 class InsertionSort:
-    #Iterative Method
-    #region
+    #region Iterative Method
     def iterativeMthd(self, lst):
         n = len(lst)
         if n < 1:
@@ -13,9 +12,24 @@ class InsertionSort:
                 j -= 1
         return lst
     #endregion
-
+    
+    #region Iterative Method
+    def recursiveMthd(self, lst, currentPos=1):
+        n = len(lst)
+        #Base consdition
+        if n<1 or currentPos>=n:
+            return lst
+        #logic
+        j = currentPos
+        while j>0 and lst[j]<lst[j-1]:
+            (lst[j], lst[j-1]) = (lst[j-1], lst[j])
+            j -= 1
+        #recursive call
+        return self.recursiveMthd(lst, currentPos +1)
+    #endregion
 
 unorderedLst1 = [3, 2, -1, 2, 4, 8]
 unorderedLst2 = [10, 2, -7, 9, 3, 21]
 sort = InsertionSort()
-print(sort.iterativeMthd([]))
+print(sort.iterativeMthd(unorderedLst1))
+print(sort.recursiveMthd(unorderedLst2))
